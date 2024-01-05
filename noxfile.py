4230994a5d1d1dc8695a9da954d3389ad8e0f29f
matho <mathonal@gmail.com>
2024-01-05 20:37:55 +0100

    first commit

---
+++ noxfile.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from typing import List
+
+import nox
+
+MODULE_NAME = "template_python"
+
+nox.options.reuse_existing_virtualenvs = True
+nox.options.error_on_missing_interpreters = True
+PYTHON_VERSIONS: List[str] = ["3.10"]
+
+@nox.session(venv_backend="virtualenv", python=PYTHON_VERSIONS[-1])
+def dev(session):
+    """Set up an environment for a developer, which can be used by an IDE as all in one.
+
+    Usage
+    -----
+    > nox -s dev
+    """
+    session.install("-r", "dev-requirements.txt")
+    session.install("-e", ".")
