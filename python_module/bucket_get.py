cb63dfed2b6eea9a3a0884ebefa0e98be49c953b
Alexis Mathon <alexis.mathon@non.se.com>
2024-01-07 22:48:59 +0100

    cloud pathlibandboto

--- python_module/bucket_get.py
+++ python_module/bucket_get.py
@@ -1,5 +1,7 @@
 import boto3
 from typing import List
+from cloudpathlib import S3Path
+import pandas as pd
 
 def get_bucket_uri_by_keyword(keyword: str) -> List[str]:
     
@@ -15,10 +17,15 @@ def get_bucket_uri_by_keyword(keyword: str) -> List[str]:
     
     return filtered_bucket_names
     
-bucket_name_list = get_bucket_uri_by_keyword("sagemaker-us")
-print(bucket_name_list)
-bucket = bucket_name_list[0]
-print(bucket)
+buckets = get_bucket_uri_by_keyword("sagemaker-us")
+print(buckets)
+bucket_name = buckets[0]
+print(bucket_name)
 
 s3_client = boto3.client("s3")
-s3_client.download_file(bucket, 'meuh.txt', 'meuh.txt')
+s3_client.download_file(bucket_name, 'meuh.txt', 'meuh.txt')
+#s3_client.download_file(bucket_name, 'path/to/file.csv', 'file.csv')
+
+input_path_uri = S3Path(f"s3://{bucket_name}") / 'meuh.txt'
+print(f"found file: {input_path_uri.exists()}")
+pd.read_csv(input_path_uri)
